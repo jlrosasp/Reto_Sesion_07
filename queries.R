95dbd01580e27f615bf724132b50c05fccecace4
Jose Luis Rosas <jlrosasp@gmail.com>
2021-02-25 03:55:13 +0000

    Se agrega conexion a MySQL desde R

--- queries.R
+++ queries.R
@@ -1,1 +1,24 @@
 #Reto1 Sesion07
+
+#install.packages("DBI")
+#install.packages("RMySQL")
+
+library(DBI)
+library(RMySQL)
+
+# Una vez que se tengan las librerias necesarias se procede a la lectura 
+# (podría ser que necesites otras, si te las solicita instalalas y cargalas), 
+# de la base de datos de Shiny la cual es un demo y nos permite interactuar con 
+# este tipo de objetos. El comando dbConnect es el indicado para realizar la 
+# lectura, los demás parametros son los que nos dan acceso a la BDD.
+
+MyDataBase <- dbConnect(
+  drv = RMySQL::MySQL(),
+  dbname = "shinydemo",
+  host = "shiny-demo.csa7qlmguqrf.us-east-1.rds.amazonaws.com",
+  username = "guest",
+  password = "guest")
+
+# Si no se arrojaron errores por parte de R, vamos a explorar la BDD
+
+dbListTables(MyDataBase)
